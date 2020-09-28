0d6a001b96e157e40e6f8f2775969e3f28cfe741
AmorFatiBA <you@example.com>
2020-09-28 11:08:09 +0300

    first commit

---
+++ Emlak.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+
+
+namespace Calisma40_ClassSample
+{
+    class Emlak
+    {
+        private string semt;
+        private string renk;
+        private int odaSayisi;
+        private int katSayisi;
+        private double alan;
+
+        public string Semti
+        {
+            get { return semt; }
+            set
+            {
+                semt=value.ToUpper();
+            }
+        }
+        public string Rengi
+        {
+            get { return renk; }
+            set
+            {
+                renk=value.ToLower();
+            }
+        }
+        public int OdaSayisi 
+        {
+            get { return odaSayisi; }
+            set
+            {
+                odaSayisi=Math.Abs(value);
+            }
+        }
+         public int KatSayisi 
+        {
+            get { return katSayisi; }
+            set
+            {
+                katSayisi=Math.Abs(value);
+            }
+        }
+         public double Alani 
+        {
+            get { return alan; }
+            set
+            {
+                alan=Math.Abs(value);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calisma40_ClassSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Emlak emlak = new Emlak();
+            emlak.Semti = "Keçiören";
+            emlak.Rengi = "Bej";
+            emlak.OdaSayisi = 4;
+            emlak.KatSayisi = 3;
+            emlak.Alani = 140;
+
+            Console.WriteLine("Evin bulunduğu semt :"+emlak.Semti);
+            Console.WriteLine("Evin rengi          :"+emlak.Rengi);
+            Console.WriteLine("Evin oda sayisi     :"+emlak.OdaSayisi);
+            Console.WriteLine("Evin kat sayisi     :"+emlak.KatSayisi);
+            Console.WriteLine("Evin alalnı         :"+emlak.Alani+"m2");
+
+            Console.Read();
+        }
+    }
+}
